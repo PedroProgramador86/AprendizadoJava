6472e00bfa28b2e2b35b8f8d08d993736b97483b
Pedro Enzo <165727452+PedroProgramador86@users.noreply.github.com>
2025-05-13 03:03:19 +0000

    Commitando pelo CodeSpace

---
+++ apresentacaoDoCodeSpace/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+
+
+    public static void main (String[]args){
+        
+        List <String> lista = new ArrayList<>();
+        lista.add("Hello World 1");
+        lista.add("Hello World 2");
+        lista.add("Hello World 3");
+        lista.add("Hello World 4");
+
+        // Percorrendo a lista: 
+
+        for(String variavelDeControle : lista){
+
+            System.out.println(lista);
+
+        }
+
+    }
+}
